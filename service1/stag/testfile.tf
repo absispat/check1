51a4f2ca542b520541225a28e1e1205bbed98466
Abhishek Patil <abhishek.p@cldcvr.com>
2020-09-18 11:24:01 +0530

    Update testfile.tf

---
+++ service1/stag/testfile.tf
@@ -0,0 +1,31 @@
+provider "google" {
+  project = "cloudcover-sandbox"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance4"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+
+terraform {
+  backend "gcs" {
+    bucket = "ovoterraformtest"
+    prefix  = "service1/stag"
+  }
+}
